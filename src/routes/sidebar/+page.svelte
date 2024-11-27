2a7949c4ddae8d4faa905fa32823c0553fdf199f
jay.jiang <jxj8868@163.com>
2024-11-27 17:40:11 +0800

    [A] input autoresize

--- src/routes/sidebar/+page.svelte
+++ src/routes/sidebar/+page.svelte
@@ -30,6 +30,7 @@
 
 	let elemChat: HTMLElement;
 	let pending: boolean = false;
+	let promptInput: HTMLElement;
 	const lorem = faker.lorem.paragraph();
 
 	// Messages
@@ -85,6 +86,16 @@
 		})
 	}
 
+	function adjustHeight() {
+		promptInput.style.height = 'auto';
+		promptInput.style.height = promptInput.scrollHeight + 'px';
+	}
+
+	function handleInput() {
+		adjustHeight();
+	}
+
+
 	async function sendMessage(message: string){
 		const userChatMessage: ChatMessage = {role: "user", content: message, type: "user", timestamp: getCurrentTimestamp()}
 		addMessage(userChatMessage)
@@ -102,6 +113,7 @@
 		if (event.keyCode === 13 && (event.ctrlKey || event.altKey)) {
 			event.preventDefault();
 			event.target.value += "\n";
+			adjustHeight();
 		} else if (event.keyCode === 13) {
 			event.preventDefault();
 			sendMessage(currentMessage);
@@ -214,7 +226,8 @@
   <title>ChatEase</title>
 </svelte:head>
 <div class="chat w-full h-screen grid grid-cols-1 lg:grid-cols-[30%_1fr]">
-  <div class="text-right px-5"><button class="anchor" on:click={onClear}>清空对话</button></div>
+  <div class="text-right px-5 py-3"><button class="anchor" on:click={onClear}>清空对话</button></div>
+  <hr />
   <!-- Chat -->
   <div class="grid grid-row-[1fr_auto]">
     <!-- Conversation -->
@@ -223,9 +236,12 @@
         {#if bubble.host === true}
           <div class="grid grid-cols-[auto_1fr] gap-2">
             <!-- <Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" /> -->
+			<div class="w-[24px] h-[24px]">
+				<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="inherit" class="eyeqlp52 st-emotion-cache-1pbsqtx ex0cdmw0"><rect width="24" height="24" fill="none"></rect><path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3zm-2 10H6V7h12v12zm-9-6c-.83 0-1.5-.67-1.5-1.5S8.17 10 9 10s1.5.67 1.5 1.5S9.83 13 9 13zm7.5-1.5c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zM8 15h8v2H8v-2z"></path></svg>
+			</div>
             <div class="card p-4 variant-soft rounded-tl-none space-y-2">
               <header class="flex justify-between items-center">
-                <p class="font-bold">{bubble.name}</p>
+                <!-- <p class="font-bold">{bubble.name}</p> -->
                 <small class="opacity-50">{bubble.timestamp}</small>
               </header>
               <p><MarkdownRenderer content={bubble.message} /></p>
@@ -235,12 +251,15 @@
           <div class="grid grid-cols-[1fr_auto] gap-2">
             <div class="card p-4 rounded-tr-none space-y-2 {bubble.color}">
               <header class="flex justify-between items-center">
-                <p class="font-bold">{bubble.name}</p>
+                <!-- <p class="font-bold">{bubble.name}</p> -->
                 <small class="opacity-50">{bubble.timestamp}</small>
               </header>
               <p><MarkdownRenderer content={bubble.message} /></p>
             </div>
             <!-- <Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" /> -->
+			<div class="w-[24px] h-[24px]">
+				<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="inherit" class="eyeqlp52 st-emotion-cache-1pbsqtx ex0cdmw0"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M10.25 13a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0zM15 11.75a1.25 1.25 0 100 2.5 1.25 1.25 0 000-2.5zm7 .25c0 5.52-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2s10 4.48 10 10zM10.66 4.12C12.06 6.44 14.6 8 17.5 8c.46 0 .91-.05 1.34-.12C17.44 5.56 14.9 4 12 4c-.46 0-.91.05-1.34.12zM4.42 9.47a8.046 8.046 0 003.66-4.44 8.046 8.046 0 00-3.66 4.44zM20 12c0-.78-.12-1.53-.33-2.24-.7.15-1.42.24-2.17.24a10 10 0 01-7.76-3.69A10.016 10.016 0 014 11.86c.01.04 0 .09 0 .14 0 4.41 3.59 8 8 8s8-3.59 8-8z"></path></svg>
+			</div>
           </div>
         {/if}
       {/each}
@@ -250,12 +269,12 @@
       <div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token w-full">
         <button class="input-group-shim">+</button>
         <textarea
+		  bind:this={promptInput}
           bind:value={currentMessage}
           class="bg-transparent border-0 ring-0"
-          name="prompt"
-          id="prompt"
           placeholder="有问题尽管问我~"
           rows="1"
+		  on:input={handleInput}
           on:keydown={onPromptKeydown}
         ></textarea>
         <button class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'} on:click={onSend}>
